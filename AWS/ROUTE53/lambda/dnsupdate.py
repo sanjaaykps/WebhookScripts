72eac4b3def084002e428347dece1b2df5e766cc
sajithapps <sajith@appranix.com>
2022-02-17 18:52:13 +0530

    Update dnsupdate.py

--- AWS/ROUTE53/lambda/dnsupdate.py
+++ AWS/ROUTE53/lambda/dnsupdate.py
@@ -1,14 +1,29 @@
+from email import message
 import json
 import boto3
 import base64
 from urllib.request import urlopen
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+message = "Something went wrong !!!"
+
+resouce_list = ["COMPUTE"]  #[COMPUTE, APPLICATION_LOADBALANCER, CLASSIC_LOADBALANCER] 
 
 client = boto3.client('route53')
 
+class DnsUpdateException(Exception):
 
-def updateRecordSetwithNewValue(hostedID, record_name, recordType, oldValue, NewValue):
+    def __init__(self, message="Some thing went wrong!!!"):
+        self.message = message
+        logger.error(message)
+        super().__init__(self.message)
+
+
+def updateRecordSetwithNewValue(hosted_id, record_name, record_type, new_ip):
     response = client.change_resource_record_sets(
-        HostedZoneId=hostedID,
+        HostedZoneId=hosted_id,
         ChangeBatch={
             "Comment": "DNS Updated Programatically",
             "Changes": [
@@ -16,11 +31,11 @@ def updateRecordSetwithNewValue(hostedID, record_name, recordType, oldValue, New
                     "Action": "UPSERT",
                     "ResourceRecordSet": {
                         "Name": record_name,
-                        "Type": recordType,
+                        "Type": record_type,
                         "TTL": 180,
                         "ResourceRecords": [
                             {
-                                "Value": NewValue
+                                "Value": new_ip
                             },
                         ],
                     }
@@ -28,84 +43,90 @@ def updateRecordSetwithNewValue(hostedID, record_name, recordType, oldValue, New
             ]
         }
     )
-    print(response)
+    logger.info(response)
+    
 
-def findandreplaceArecord(sourceIP, targetIP):
-    # hostedZones = list_all_hostedzone_response = 
-    list_all_hostedzone_response = client.list_hosted_zones()
+def find_and_replace_a_records(findsourceip, replacetargetip):
+    try:
+        list_all_hostedzone_response = client.list_hosted_zones()
+    except Exception as e:
+        logger.error(f"Failed to get the list of all hosted zones {e}")
+        raise e
+        
     if list_all_hostedzone_response['ResponseMetadata']['HTTPStatusCode'] == 200:
         hosted_zones = list_all_hostedzone_response['HostedZones']
+        if len(hosted_zones) == 0:
+            message = "There are no Hosted Zones available"
+            raise DnsUpdateException(message)                           
         for zone in hosted_zones:
-            print(zone)
-            #find if source IP is the value of the A record in any of the hosted zones
-            hostedZoneDetails = client.list_resource_record_sets(HostedZoneId=zone['Id'])
-            if hostedZoneDetails != "":
-                for recordset in hostedZoneDetails['ResourceRecordSets']:
-                    if recordset['Type'] == 'A':
-                        for resourcerecord in recordset['ResourceRecords']:
-                            if resourcerecord['Value'] == sourceIP:
-                                print(recordset)
-                                updateRecordSetwithNewValue(zone['Id'], recordset['Name'], 'A', sourceIP, targetIP)
+            logger.debug(f"Hosted Zone Detail = {zone}")
+            #List all the record set in the Hostedzone
+            resource_record_sets = client.list_resource_record_sets(HostedZoneId=zone['Id'])
+            if len(resource_record_sets) == 0:
+                message = "There are no resource record sets found"
+                raise DnsUpdateException(message)  
+            for recordset in resource_record_sets['ResourceRecordSets']:
+                if recordset['Type'] == 'A':
+                    logger.debug("Found a A Record set")
+                    for resourcerecord in recordset['ResourceRecords']:
+                        if resourcerecord['Value'] == findsourceip:
+                            logger.debug(recordset)
+                            updateRecordSetwithNewValue(zone['Id'], recordset['Name'], 'A', replacetargetip)
+    else:
+        message = "Got an upexpcted response when trying to list hostedzone \
+            repsonse" + str(list_all_hostedzone_response['ResponseMetadata']['HTTPStatusCode'])
+        raise DnsUpdateException(message)                           
 
 
-def updateARecord(listofdictofips):
+def update_a_record(listofdictofips):
     for each_item in listofdictofips:
-        sourceIP = each_item['sourceip']
-        targetIP = each_item['targetip']
-        findandreplaceArecord(sourceIP, targetIP)
+        source_ip = each_item['sourceip']
+        target_ip = each_item['targetip']
+        logger.info(f"Source IP  = {source_ip} and Target IP = {target_ip}")
+        find_and_replace_a_records(source_ip, target_ip)
     
-# process_dict = []
-# url = "https://storage.googleapis.com/appranixsra-devorg/recovery/53:devorg:6d81b34312da:97660c9559ad:1644860938670:6427d46ba5fb:c45d40470484/resource-mapping/cross6-metadata-changes.json?X-Goog-Algorithm=GOOG4-RSA-SHA256&X-Goog-Credential=axproduction-53%40appranixsra.iam.gserviceaccount.com%2F20220215%2Fauto%2Fstorage%2Fgoog4_request&X-Goog-Date=20220215T152349Z&X-Goog-Expires=14400&X-Goog-SignedHeaders=host&X-Goog-Signature=9f5ff48a03fe6b8a8dd374ad54fc6f8a5933aabfd3ed7c1dbf34f85f6ce9da9edd842c84bcf2de89326901356fba191a378c03566da805120c5553a7a239474e9fd3fd64edefeed7bf340bf7d4e4aa6e794f24d98755ceac9ff13f32543d8d7af2980ce3349120bf1d16577d40a981fc64795aa876fb8536437e39a96cdf8236d5faec7f12ad88498c976de53341338de19bd55c1719ffd4974b9ad159fc511d2f4e41faf1b724203b87f966f9b25fba709c4fe45382b71fb8e37dbede953d71b6214a4d0b4df6bea44a3b0f4f88e510fb4943ff6e704184454eae759552801af0164152ac5556136332b6903ea64dcecb2ffa81527c3c1ecc528e03926fc08f"
-# response = urlopen(url)
-# data_json = json.loads(response.read())
-# for dat in data_json:
-#     if "COMPUTE" in dat.keys():
-#         dat_values = list(dat.values())
-#         data_dict = dat_values[0][0]
-        
-#         for k, v in data_dict.items():
-#             sourceip = v['source']['publicIpAddress']
-#             destip = v['destination']['publicIpAddress']
-#             ip_dict = {"sourceip" : sourceip, "targetip" : destip}
-#             process_dict.append(ip_dict)
-#             print(process_dict)
-            
-# if len(process_dict) != 0:
-#     updateARecord(process_dict)
-# else:
-#     print("FAILED")
 
 def lambda_handler(event, context):
-    data = base64.b64decode(event["body-json"]).decode('utf-8').replace('null', '"user"')
-    print(data)
-    data_dictionary = eval(data)
-    process_dict = []
-    if data_dictionary['recoveryStatus'] == "RECOVERY_COMPLETED":
-        url = data_dictionary["resourceMapping"]["sourceRecoveryMappingPath"]
-
-        response = urlopen(url)
-        data_json = json.loads(response.read())
-        for dat in data_json:
-            if "COMPUTE" in dat.keys():
-                dat_values = list(dat.values())
-                data_dict = dat_values[0][0]
-                for k, v in data_dict.items():
-                    sourceip = v['source']['publicIpAddress']
-                    destip = v['destination']['publicIpAddress']
-                    ip_dict = {"sourceip" : sourceip, "targetip" : destip}
-                    process_dict.append(ip_dict)
-                    print(process_dict)
-                    
-        if len(process_dict) != 0:
-            updateARecord(process_dict)
-            return {
-                'statusCode': 200,
-                'body': {"Status": "Success"}
-            }
+    try:
+        data = base64.b64decode(event["body-json"]).decode('utf-8').replace('null', '"user"')
+        logger.info(data)
+        data_dictionary = eval(data)
+        process_dict = []
+        if data_dictionary['recoveryStatus'] == "RECOVERY_COMPLETED":
+            url = data_dictionary["resourceMapping"]["sourceRecoveryMappingPath"]
+            response = urlopen(url)
+            source_recovery_resource_mapping_json = json.loads(response.read())
+            for source_recovery_map in source_recovery_resource_mapping_json:
+                for resource_type in resouce_list:
+                    if resource_type in source_recovery_map.keys():
+                        logger.debug(f"Resource Type {resource_type}")
+                        dat_values = list(source_recovery_map.values())
+                        data_dict = dat_values[0][0]
+                        for k, v in data_dict.items():
+                            sourceip = v['source']['publicIpAddress']
+                            destip = v['destination']['publicIpAddress']
+                            ip_dict = {"sourceip" : sourceip, "targetip" : destip}
+                            process_dict.append(ip_dict)
+                            logger.info(process_dict)
+                        
+            if len(process_dict) != 0:
+                update_a_record(process_dict)
+                return {
+                    'statusCode': 200,
+                    'body': {"Status": "Success"}
+                }
+            else:
+                logger.error(f"Failed as there are no source and target ip map found")
+                return {
+                    'statusCode': 505,
+                    'body': {"Status": "Failed"}
+                }
         else:
-            print("FAILED")
+            message = "The Recovery did not complete successfully so skipping DNS Update"
+            raise DnsUpdateException(message)
+    except Exception as e:
+            logger.error(f"FAILED with Exception {e}")
             return {
                 'statusCode': 505,
-                'body': {"Status": "Success"}
+                'body': {"Status": "Failed", "Message": "Failed" }
             }
-            
